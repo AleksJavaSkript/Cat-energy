20a9e7612e99326fdf97db63f54fa6a06b4dfdf5
Aleksandr <vsegda.bez.granic@gmail.com>
2024-04-11 18:30:35 +0300

    html min

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
 import del from 'del';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
 
 // Styles
 
@@ -32,8 +33,10 @@ export const styles = () => {
 
 // HTML
 
-const html = () => {
+
+export const html = () => {
   return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('build'));
 }
 
