053c1e643062857cb82efe87fdbb3d5e9022ed9a
Aleksandr <vsegda.bez.granic@gmail.com>
2024-05-02 20:26:58 +0300

    2 защита

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector('.nav');
 const menuBtn = document.querySelector('.nav__toggle');
 
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
+}
+
 menuBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
   menu.classList.toggle('nav--open');
